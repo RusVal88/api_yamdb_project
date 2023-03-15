17cce6272af6c80f0f25f7c20fdb2e8740c8a116
minin_dm <minin_dm@mail.ru>
2023-03-15 16:32:29 +0300

    Прописаны модели Review, Comment, реализован подсчёт avg рейтинга, прописан сериализатор для вывода данного рейтинга

---
+++ api_yamdb/api/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from review.models import Category, Genre, Titles, Review, Comment
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Titles)
+admin.site.register(Review)
+admin.site.register(Comment)
