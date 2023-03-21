6766735718b55c1cb2b7ca77a14ef4f7798ff39f
minin_dm <minin_dm@mail.ru>
2023-03-21 22:24:18 +0300

    устранение конфликтов

---
+++ api_yamdb/users/validators.py
@@ -0,0 +1,15 @@
+import re
+
+from rest_framework.validators import ValidationError
+
+
+def validate_username(value):
+    if value.lower() == 'me':
+        raise ValidationError(
+            '"me" в качестве логина использовать нельзя!'
+        )
+    if not re.match(r'^[\w.@+-]+\Z', value):
+        raise ValidationError(
+            'Введены некоректные символы!'
+        )
+    return value
